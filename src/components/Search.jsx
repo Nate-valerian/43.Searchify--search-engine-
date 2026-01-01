de9d196809a7b5cc9482817c9539bd21253398b1
Nate Valerian <n.nate1544@gmail.com>
2026-01-01 16:16:23 +0300

    node.jd update, api updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,19 +11,23 @@ export const Search = () => {
 
   useEffect(() => {
     if (debouncedValue) setSearchTerm(debouncedValue);
-  }, [debouncedValue]);
+  }, [debouncedValue, setSearchTerm]); // Added setSearchTerm to dependencies
 
   return (
     <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
       <input
         value={text}
         type="text"
-        className="sm:w-96 w-80 h-10 dark:bg-gray-200  border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
         placeholder="🔎 Search Google or type URL"
         onChange={(e) => setText(e.target.value)}
       />
       {text !== '' && (
-        <button type="button" className="absolute top-1.5 right-4 text-2xl text-gray-500 " onClick={() => setText('')}>
+        <button
+          type="button"
+          className="absolute top-1.5 right-4 text-2xl text-gray-500"
+          onClick={() => setText('')}
+        >
           x
         </button>
       )}
@@ -31,3 +35,4 @@ export const Search = () => {
     </div>
   );
 };
+
